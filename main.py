646dba1fc03ae58940d0facfbb57af90c0b20a06
thefirebanks <dfireban@oberlin.edu>
2020-04-09 23:06:34 -0400

    added train test split

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ python main.py -f data/cubic_nC3_nV10_nP100_imC40.profiles -c 3 -e 100 -opt Adam
 from automated_voting.algorithms.neural_network import AVNet
 from automated_voting.voting.profiles import load_dataset, AVProfile
 from automated_voting.voting.election import evaluate_baselines
+import random
+
+
 import time
 import argparse
 
@@ -32,8 +35,22 @@ parser.add_argument('-lr', '--l_rate', metavar='lrate', type=float,
                     default=0.01,
                     help="learning rate for network")
 
+parser.add_argument('-tp', '--t_perc', metavar='tperc', type=float,
+                    default=0.2,
+                    help="test percentage")
+
+parser.add_argument('-s', '--seed', metavar='seed', type=float,
+                    default=69420,
+                    help="seed")
 
 args = parser.parse_args()
+
+
+def train_test_split(X, test_size, seed):
+    random.shuffle(X, seed=seed)
+    test_start_idx = len(X) - test_size*len(X)
+    return X[:test_start_idx], X[test_start_idx]
+
 
 def main():
     # Step 1: Define basic parameters
@@ -42,9 +59,14 @@ def main():
     epochs = args.epochs
     l_rate = args.l_rate
     opt = args.optimizer
+    seed = args.seed
+    t_perc = args.t_perc
 
     # Step 2: Load a dataset - a list of AVProfiles
     profiles = load_dataset(args.file_name)
+
+    # Step 2.1: Split into train test sets
+    profiles_train, profiles_test = train_test_split(profiles, test_size=t_perc, seed=seed)
 
     # Step 3: Run baseline models to compare later
     start_b = time.time()
@@ -57,7 +79,12 @@ def main():
     # Step 4: Define model and start training loop
     start_n = time.time()
     av_model = AVNet(n_features, n_candidates, inp_shape=(1, n_features), opt=opt, l_rate=l_rate)
-    av_model.train(profiles, epochs)
+
+    # This is the equivalent to model.fit() or model.train()
+    av_model.train(profiles_train, epochs)
+
+    # This is more of the equivalent of model.predict() - the reason being that the train method
+    av_model.train(profiles_test, epochs=1)
 
     _ = av_model.get_results()
 
